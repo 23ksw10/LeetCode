eab47ddafd9edede4addfee615674e97ea1edeca
23ksw10 <kimsw9603@gmail.com>
2020-02-05 01:13:21 +0900

    C++

---
+++ algorithm/1108_Defanging an IP Address/solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+class Solution {
+public:
+	string defangIPaddr(string address) {
+		string ans;
+		for (int i = 0; i < address.size(); i++)
+		{
+
+			if (address[i] == '.')
+			{
+				ans.push_back('[');
+				ans.push_back('.');
+				ans.push_back(']');
+			}
+			else
+				ans.push_back((address[i]));
+		}
+		return ans;
+	}
+};
