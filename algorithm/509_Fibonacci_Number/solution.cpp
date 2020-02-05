471ab13632ce8b60dc0f04c449416a8226d9847d
23ksw10 <kimsw9603@gmail.com>
2020-02-05 21:23:23 +0900

    C++

---
+++ algorithm/509_Fibonacci_Number/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+
+
+class Solution {
+public:
+	int fib(int N) {
+		if (N == 0)
+			return 0;
+		else if (N == 1)
+			return 1;
+		else
+			return fib(N - 1) + fib(N - 2);
+	}
+};
