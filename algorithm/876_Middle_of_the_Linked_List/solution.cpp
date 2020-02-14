12cf21cf7c86e9325d9876aa324e0a5847d23631
23ksw10 <kimsw9603@gmail.com>
2020-02-15 01:34:20 +0900

    C++

---
+++ algorithm/876_Middle_of_the_Linked_List/solution.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+	ListNode* middleNode(ListNode* head) {
+		if (!head && !head->next)
+			return head;
+		ListNode* slow = head;
+		ListNode* fast = head;
+		while (fast&&fast->next)
+		{
+			slow = slow->next;
+			fast = fast->next->next;
+		}
+		return slow;
+
+	}
+
+
+
+};
