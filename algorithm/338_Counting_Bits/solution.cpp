77e77ce566c80fc7fa100a79159d93ffd3424dd2
23ksw10 <kimsw9603@gmail.com>
2020-02-18 21:19:54 +0900

    C++

---
+++ algorithm/338_Counting_Bits/solution.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+
+
+using namespace std;
+
+
+
+class Solution {
+public:
+	vector<int> countBits(int num) {
+
+		vector<int>ans(num + 1, 0);
+
+		for (int i = 1; i <= num; i++)
+		{
+			ans[i] = ans[i&(i - 1)] + 1;
+		}
+
+
+		return ans;
+
+
+
+	}
+};
