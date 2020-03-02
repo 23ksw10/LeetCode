ce99a6a4aacbdd0166aa7cefa5a0985e44fac1db
23ksw10 <kimsw9603@gmail.com>
2020-03-03 00:41:37 +0900

    C++

---
+++ algorithm/1306_Jump_Game_III/solution.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+
+
+
+
+
+using namespace std;
+
+
+
+
+class Solution {
+public:
+	unordered_set<int> vis;
+	bool canReach(vector<int>& arr, int start) {
+		if (start >= 0 && start < arr.size() && vis.insert(start).second) {
+			return arr[start] == 0 || canReach(arr, start + arr[start]) || canReach(arr, start - arr[start]);
+		}
+		return false;
+
+	}
+
+
+
+};
