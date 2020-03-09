be606119d95918170cda7976d950962bac6711eb
23ksw10 <kimsw9603@gmail.com>
2020-03-10 02:37:45 +0900

    C++

---
+++ algorithm/917_Reverse_Only_Letters/solution.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stack>
+#include <strign>
+using namespace std;
+
+
+class Solution {
+public:
+	string reverseOnlyLetters(string S) {
+		stack<char> letters;
+		string ans;
+		for (int i = 0; i < S.size(); i++)
+		{
+			if (S[i] >= 'a'&&S[i] <= 'z')letters.push(S[i]);
+			if (S[i] >= 'A'&&S[i] <= 'Z')letters.push(S[i]);
+		}
+		for (int i = 0; i < S.size(); i++)
+		{
+			if (S[i] >= 'A'&&S[i] <= 'Z')
+			{
+				ans.push_back(letters.top());
+				letters.pop();
+			}
+			else if (S[i] >= 'a'&&S[i] <= 'z')
+			{
+				ans.push_back(letters.top());
+				letters.pop();
+			}
+			else ans.push_back(S[i]);
+		}
+		return ans;
+
+	}
+};
