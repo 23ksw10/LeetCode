2a30d08c72f5b19108cfd5e598e2964e66554964
23ksw10 <kimsw9603@gmail.com>
2020-07-29 23:43:46 +0900

    C++

---
+++ algorithm/413_Arithmetic_Slices/solution.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+
+
+using namespace std;
+
+
+
+class Solution {
+public:
+	int numberOfArithmeticSlices(vector<int>& A) {
+		int ans = 0;
+		if (A.size() < 3)return 0;
+		int dp;
+		for (int i = 2; i < A.size(); i++) {
+			if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+				dp++;
+			}
+			else {
+				dp = 0;
+			}
+			ans += dp;
+		}
+		return ans;
+
+
+	}
+};
