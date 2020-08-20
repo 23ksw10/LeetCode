bd40f907b4baafbbbe53159c49ffa117c7d53bd8
23ksw10 <kimsw9603@gmail.com>
2020-08-20 23:39:00 +0900

    C++

---
+++ algorithm/454_4Sum_II/solution.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+
+
+using namespace std;
+
+class Solution {
+public:
+	int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+		int size = A.size();
+		unordered_map<int, int>m;
+		for (auto a : A) {
+			for (auto b : B) {
+				m[a + b]++;
+			}
+		}
+		int count = 0;
+		for (auto c : C) {
+			for (auto d : D) {
+				if (m.find(-c - d) != m.end()) count += m[-(c + d)];
+			}
+		}
+		return count;
+	}
+};
