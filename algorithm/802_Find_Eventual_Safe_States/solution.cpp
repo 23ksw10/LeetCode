2ee997157c37f28f7842f5c62a9f4c1b4abf0623
Kim sun wook <50804079+23ksw10@users.noreply.github.com>
2020-09-02 02:33:29 +0900

    Update solution.cpp

--- algorithm/802_Find_Eventual_Safe_States/solution.cpp
+++ algorithm/802_Find_Eventual_Safe_States/solution.cpp
@@ -14,24 +14,23 @@ using namespace std;
 
 class Solution {
 public:
-	bool dfs(vector<vector<int>>& graph, int v, vector<int>& vis) {
-		if (vis[v] != 0)
-			return vis[v] == 1;
-		vis[v] = 2;
-		for (auto it = graph[v].begin(); it != graph[v].end(); it++)
-			if (!dfs(graph, *it, vis))
-				return false;
-		vis[v] = 1;
-		return true;
-	}
-	vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-		int n = graph.size();
-		vector<int>res;
-		vector<int>vis(n, 0);
-		for (int i = 0; i < n; i++) {
-			if (dfs(graph, i, vis))
-				res.push_back(i);
-		}
-		return res;
-	}
+    bool dfs(vector<vector<int>>& graph,vector<int>&check,int start){
+        if(check[start]!=0)
+            return check[start]==1;
+        check[start]=2;
+        for(int i=0;i<graph[start].size();i++){
+            if(!dfs(graph,check,graph[start][i]))
+                return false;
+        }
+        check[start]=1;
+        return true;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        vector<int>check(graph.size(),0);
+        vector<int>ans;
+        for(int i=0;i<graph.size();i++){
+            if(dfs(graph,check,i)) ans.push_back(i);
+        }
+        return ans;
+    }
 };
