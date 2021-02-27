ba7f728a0378ec0d673245bb786bc95cd8ee5cf3
23ksw10 <kimsw9603@gmail.com>
2021-02-27 21:36:28 +0900

    C++

---
+++ algorithm/1267_Count_Servers_that_Communicate/solution.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+
+
+
+
+using namespace std;
+
+class Solution {
+public:
+	int countServers(vector<vector<int>>& grid) {
+		int ans = 0;
+		int m = grid.size();
+		int n = grid[0].size();
+		vector<int>row(m, 0);
+		vector<int>col(n, 0);
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (grid[i][j]) {
+					row[i]++;
+					col[j]++;
+				}
+			}
+		}
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (grid[i][j] && (col[j] > 1 || row[i] > 1)) {
+					ans++;
+				}
+			}
+		}
+		return ans;
+
+	}
+};
