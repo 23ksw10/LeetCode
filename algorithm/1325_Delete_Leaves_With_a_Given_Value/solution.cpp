6f7f93e250f095626690d7798507d4f56d1c591f
github 23ksw10 <kimsw9603@gmail.com>
2022-03-14 21:32:11 +0900

    C++

---
+++ algorithm/1325_Delete_Leaves_With_a_Given_Value/solution.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+    
+};
+ 
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if (!root)return root;
+        root->left = removeLeafNodes(root->left, target);
+        root->right = removeLeafNodes(root->right, target);
+        if (root->val == target) {
+            if (!root->left && !root->right)return nullptr;
+        }
+        return root;
+    }
+};
