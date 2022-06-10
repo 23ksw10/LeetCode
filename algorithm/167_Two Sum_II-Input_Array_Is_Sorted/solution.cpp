5a40439b116a162301eac692c465c0cd34986709
github 23ksw10 <kimsw9603@gmail.com>
2022-06-10 22:28:50 +0900

    C++

---
+++ algorithm/167_Two Sum_II-Input_Array_Is_Sorted/solution.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+
+
+
+using namespace std;
+
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& numbers, int target) {
+		vector<int>ans;
+		int l = 0;
+		int r = numbers.size() - 1;
+		while (l < r) {
+			if (numbers[l] + numbers[r] == target) {
+				ans.push_back(l + 1);
+				ans.push_back(r + 1);
+				break;
+			}
+			else if (numbers[l] + numbers[r] > target) {
+				r--;
+			}
+			else {
+				l++;
+			}
+		}
+		return ans;
+	}
+};
